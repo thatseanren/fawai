f5db173103e0d8a8b53714d3674632153073ab8f
thatseanren <seanren96@gmail.com>
2021-05-27 08:46:49 +0800

    fasd

---
+++ pages/taskdetail/[_id].tsx
@@ -0,0 +1,219 @@
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
+import Header from "../header.js";
+import Button from "@material-ui/core/Button";
+import Tag from "../../styles/DataSet.module.css";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { ServerResponse } from "http";
+// import Autocomplete from '@material-ui/lab/Autocomplete';
+// import TextField from '@material-ui/core/TextField';
+import ip, { option } from "../main_config";
+import qs from "qs";
+export default function DetailsWrapper(props) {
+  const route = useRouter();
+  const { _id } = route.query;
+  console.log(_id);
+  // return <TagDetails {...props} TaskId={_id} />;
+  return <TagDetails {...props} TaskId={_id} />;
+}
+DetailsWrapper.getInitialProps = (appContext) => {
+  return { _id: appContext.query._id };
+};
+
+class TagDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openlist: 0,
+      data: {},
+    };
+  }
+  componentDidMount() {
+    console.log(this.props.TaskId);
+    axios
+      .get(`${ip}${option.getTaskList}?_id=${this.props.TaskId}`)
+      .then((res) => {
+        console.log(res.data.data[0]);
+        this.setState({ data: res.data.data[0] });
+      });
+  }
+
+  render() {
+    return (
+      <div className={Tag.tagHome}>
+        <Header />
+        <div className={Tag.homeTop}>
+          <div className={Tag.tagListLeft}>
+            <div className={Tag.basicInfoWindow}>
+              <div className={Tag.tgaTitle}>项目：{this.state.data.name}</div>
+              <div className={Tag.tagTopList}>
+                <div className={Tag.topListBox}>
+                  <div className={Tag.listBoxTitle}>基本信息</div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>标注类型：</div>
+                    <div className={Tag.boxSpanRight}>
+                      {this.state.data.type}
+                    </div>
+                  </div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>创建时间：</div>
+                    <div className={Tag.boxSpanRight}>
+                      {this.state.data.create_time}
+                    </div>
+                  </div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>创建人：</div>
+                    <div className={Tag.boxSpanRight}>Graviti_851035</div>
+                  </div>
+                </div>
+                <div className={Tag.topListBox}>
+                  <div className={Tag.listBoxTitle}>标注信息</div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>数据量：</div>
+                    <div className={Tag.boxSpanRight}>
+                      {this.state.data.num}
+                    </div>
+                  </div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>精度要求：</div>
+                    <div className={Tag.boxSpanRight}>-</div>
+                  </div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>标注标签：</div>
+                    <div className={Tag.boxSpanRight}>
+                      {this.state.data.tags}
+                    </div>
+                  </div>
+                </div>
+                <div className={Tag.topListBox}>
+                  <div className={Tag.listBoxTitle}>标注成员</div>
+                  <div className={Tag.listBoxSpan}>
+                    <div className={Tag.boxSpanLeft}>成员：</div>
+                    <div className={Tag.boxSpanRight}>1个</div>
+                  </div>
+                  <div className={Tag.listBoxSpan}>
+                    <Button variant="contained" size="large" color="primary">
+                      邀请成员
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={Tag.basicInfoWindow} style={{ marginTop: "16px" }}>
+              <div className={Tag.tgaTitle}>任务列表</div>
+              <div>
+                <div className={Tag.tableTitle}>
+                  <div style={{ flex: "2 1 0%" }}>编号</div>
+                  <div style={{ flex: "2 1 0%" }}>数据量</div>
+                  <div style={{ flex: "3 1 0%" }}>标注进度</div>
+                  <div style={{ flex: "6 1 0%" }}>标注员</div>
+                  <div style={{ flex: "3 1 0%" }}>操作</div>
+                </div>
+                <div className={Tag.tableList}>
+                  <div style={{ flex: "2 1 0%" }}>1</div>
+                  <div style={{ flex: "2 1 0%" }}>300张</div>
+                  <div style={{ flex: "3 1 0%" }}>2/300</div>
+                  <div style={{ flex: "6 1 0%" }}>Admin</div>
+                  <div style={{ flex: "3 1 0%" }}>
+                    <Button variant="outlined" color="primary">
+                      标注
+                    </Button>
+                  </div>
+                </div>
+                <div className={Tag.tableList}>
+                  <div style={{ flex: "2 1 0%" }}>2</div>
+                  <div style={{ flex: "2 1 0%" }}>300张</div>
+                  <div style={{ flex: "3 1 0%" }}>2/300</div>
+                  <div style={{ flex: "6 1 0%" }}>Admin</div>
+                  <div style={{ flex: "3 1 0%" }}>
+                    <Button variant="outlined" color="primary">
+                      标注
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={Tag.tagListRight}>
+            <div className={Tag.tgaTitle}>项目进度</div>
+            <div>
+              <div className={Tag.rigBox}>
+                <div
+                  style={{
+                    marginBottom: "16px",
+                    fontSize: "14px",
+                    color: "rgb(137, 138, 150)",
+                  }}
+                >
+                  待标注
+                </div>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div>数据量</div>
+                  <div>596</div>
+                </div>
+                <div>
+                  {/* <Autocomplete
+                                    id="combo-box-demo"
+                                    options={top100Films}
+                                    getOptionLabel={(option) => option.title}
+                                    style={{ width: 300 }}
+                                    renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+                                /> */}
+                </div>
+              </div>
+              <div className={Tag.rigBox}>
+                <div
+                  style={{
+                    marginBottom: "16px",
+                    fontSize: "14px",
+                    color: "rgb(137, 138, 150)",
+                  }}
+                >
+                  已完成
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    marginBottom: "16px",
+                  }}
+                >
+                  <div>数据量</div>
+                  <div>2</div>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    marginBottom: "16px",
+                  }}
+                >
+                  <div>标注数</div>
+                  <div>3</div>
+                </div>
+                <div style={{ marginBottom: "8px", overflow: "hidden" }}>
+                  <div style={{ float: "left" }}>
+                    <Button
+                      variant="contained"
+                      size="large"
+                      color="primary"
+                      style={{ height: "35px" }}
+                    >
+                      下载
+                    </Button>
+                  </div>
+                  <div style={{ float: "right" }}>
+                    <Button variant="outlined">同步标注结果到数据集</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- pages/tools/annotation/index.tsx
+++ pages/tools/annotation/index.tsx
@@ -115,7 +115,7 @@ const useStyles = makeStyles({
     flexFlow: "wrap",
   },
   taskPaper: {
-    minWidth:"300px",
+    minWidth: "300px",
     margin: "8px",
     position: "relative",
     // https://css-tricks.com/almanac/selectors/n/nth-of-type/
@@ -222,10 +222,9 @@ export const Body: React.FC<{}> = (props) => {
       .get(`${ip}${option.getTaskList}`)
       .then((response) => {
         if (JSON.stringify(response.data.data) === JSON.stringify(meta)) {
-         
         } else {
           response.data.data.forEach((e, i) => {
-            console.log(e.status)
+            console.log(e);
             fulllist[i] = (
               <PaperLikeTask
                 tags={e.tags}
@@ -234,6 +233,7 @@ export const Body: React.FC<{}> = (props) => {
                 Create_time={e.create_time}
                 Team={"fsda"}
                 TaskStatus={e.status}
+                Id={e._id}
               />
             );
             if (e.status === 0) {
@@ -245,6 +245,7 @@ export const Body: React.FC<{}> = (props) => {
                   Create_time={e.create_time}
                   Team={"fdsa"}
                   TaskStatus={e.status}
+                  Id={e._id}
                 />
               );
             }
@@ -286,48 +287,37 @@ export const Body: React.FC<{}> = (props) => {
             <PagesIcon style={{ fontSize: "16px", marginRight: "8px" }} />
             服务介绍
           </Button>
-<<<<<<< HEAD:pages/tools/annotation/index.tsx
-          {/* 新建标注项目 */}
-          {/* <Link href={`/tools/annotation/creation`}> */}
-          <Button
-=======
           <Link href="../addTagGing">
-              <a>
+            <a>
               <Button
->>>>>>> 08a1a20... 新建标注:component/annotationTasks.tsx
-            color="primary"
-            variant="contained"
-            className={classes.annotation}
-            onClick={() => {
-              axios
-                .post(
-                  `${ip}${option.createTask}`,
-                  qs.stringify({
-                    _id: "60a5cd03970dbe2236d071c6",
-                    name: "数据平台试验_432",
-                    tags: "5dbox",
-                    type: "como",
-                  
-                  })
-                )
-                .then((res) => {
-                  alert("successful");
-                  console.log(res);
-                })
-                .catch((err) => {
-                  alert(err);
-                });
-            }}
-          >
-            {" "}
-            新建标注项目
-          </Button>
-<<<<<<< HEAD:pages/tools/annotation/index.tsx
-          {/* </Link> */}
-=======
-          </a>
-          </Link> 
->>>>>>> 08a1a20... 新建标注:component/annotationTasks.tsx
+                color="primary"
+                variant="contained"
+                className={classes.annotation}
+                onClick={() => {
+                  axios
+                    .post(
+                      `${ip}${option.createTask}`,
+                      qs.stringify({
+                        _id: "60a5cd03970dbe2236d071c6",
+                        name: "数据平台试验_432",
+                        tags: "5dbox",
+                        type: "como",
+                      })
+                    )
+                    .then((res) => {
+                      alert("successful");
+                      console.log(res);
+                    })
+                    .catch((err) => {
+                      alert(err);
+                    });
+                }}
+              >
+                {" "}
+                新建标注项目
+              </Button>
+            </a>
+          </Link>
         </Grid>
       </Grid>
       {/* 标注项目Grid */}
@@ -371,10 +361,12 @@ const PaperLikeTask = (props: {
   Create_time: string;
   Quantity: string;
   TaskStatus: string;
+  Id:string
 }) => {
   const classes = useStyles();
+  console.log(props.Id)
   return (
-    <Link href={`/taskdetail`} style={{flexBasis:"300px"}}>
+    <Link href={`/taskdetail/${props.Id}`} style={{ flexBasis: "300px" }}>
       <Paper className={clsx(classes.taskPaper)}>
         <Grid container>
           <Grid item> {props.Name || "Sample"} </Grid>
