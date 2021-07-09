c7f18f655abd339d9ceec00b74174fdde4ece2fd
thatseanren-数据平台开发部 <rensiyang1@faw.com.cn>
2021-07-09 10:22:11 +0800

    test

--- pages/2DAnnotator/index.tsx
+++ pages/2DAnnotator/index.tsx
@@ -11,7 +11,8 @@ import SaveIcon from '@material-ui/icons/Save';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { useRouter } from 'next/router'
 interface taskInfo {
   taskid: string;
   sequence: number;
